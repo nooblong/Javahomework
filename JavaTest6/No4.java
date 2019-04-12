23b271a963097bee7e43430b4dda709f47c14ed2
nooblong <18128966990@163.com>
2019-04-12 12:21:41 +0800

    修复了一个重要bug

--- JavaTest6/No4.java
+++ JavaTest6/No4.java
@@ -6,7 +6,7 @@ public class No4 {
         int year;
         int month;
         Calendar calendar = Calendar.getInstance();
-        calTheSame(calendar);
+        calTheSame(calendar, 2008);
         System.out.println("输入年份");
         year = javain();
         System.out.println("输入月份");
@@ -53,29 +53,39 @@ public class No4 {
         return scanner.nextInt();
     }
 
-    private static void calTheSame(Calendar calendar) {
+    private static void calTheSame(Calendar calendar, int year_to_cal) {
         //2008年日期的个位数与相应的星期刚好相同的总天数
         //2008年9月1日恰好是星期一
         //所以根据题意，7为星期日
-        calendar.set(Calendar.YEAR,2008);
-        int sum = 0;
-        System.out.println("2008年各位数与相应星期相等的天数如下");
+        //所以当日期个位数为7且为星期天的时候，总数加1
+        int all_sum = 0;
+        calendar.set(Calendar.YEAR, year_to_cal);
+        System.out.println(year_to_cal + "年各位数与相应星期相等的天数如下");
         for (int i = 0; i < 12; i++) {//i为月分
+            int sum = 0;//用于计算当月有多少天
             calendar.set(Calendar.MONTH, i);
-            System.out.println("月份" + (i+1));
+            System.out.print("月份" + (i + 1) + "   ");
             int dayMax = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-            //0是星期天，1是星期一
             for (int j = 1; j < dayMax; j++) {
-                calendar.set(Calendar.DATE,j);
-                if(j>=10)
-                    if(j%10 == calendar.get(Calendar.DAY_OF_WEEK)-1)
-                        sum++;
-                else
-                    if(j == calendar.get(Calendar.DAY_OF_WEEK)-1)
-                        sum++;
+                //dayMax的二月份是错的,固定为31,不过影响不大
+                calendar.set(Calendar.DATE, j);
+                //System.out.println(getDayOfWeek(calendar) +" " +j%10);
+                if (j % 10 == getDayOfWeek(calendar))
+                    sum++;
             }
-            System.out.println("总共有"+sum+"天");
+            System.out.println("总共有" + sum + "天");
+            all_sum += sum;
+            System.out.println();
         }
-        System.out.println("故总共有" + sum + "天");
+        System.out.println("故总共有" + all_sum + "天");
+    }
+
+    private static int getDayOfWeek(Calendar calendar) {
+        //(DAY_OF_WEEK-1)后0是星期天，1是星期一
+        //所以要把星期天转成7
+        if (calendar.get(Calendar.DAY_OF_WEEK) - 1 == 0)
+            return 7;
+        else
+            return calendar.get(Calendar.DAY_OF_WEEK) - 1;
     }
 }
